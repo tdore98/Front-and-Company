fb9b68bc2148f501d79bd74f96337f8d7b2083e8
Theo <tatang@sfu.ca>
2021-04-28 16:40:46 -0700

    Testing Consignment Appointment

---
+++ js/consignment-appointment.js
@@ -0,0 +1,3 @@
+document.getElementById("consignment-appointment").addEventListener("click", function() {
+    window.location.href = "https://calendly.com/consignment-drop-off/consignment-dropoff";
+})
